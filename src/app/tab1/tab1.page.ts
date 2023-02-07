ceba5e5896b2512f2d13725a66f0c6b2bcda871a
akmelalee <akmelalee@gmail.com>
2023-02-07 17:03:21 +0500

    initial project

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+  type = 'images';
+  recent = 'images';
+  constructor() {}
+
+}
